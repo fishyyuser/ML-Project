5c2f0438d80a18d84b19c967f2fdceece7d92b2a
fishyyuser <pradybisht@gmail.com>
2025-10-23 18:19:26 +0530

    HyperParameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -5,11 +5,12 @@ import numpy as np
 import pandas as pd
 
 from src.exception import CustomException
+from src.logger import logging
 
 import dill
-import pickle
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -23,12 +24,18 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,params):
     try:
         report = {}
 
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
+        for model_name, model in models.items():
+            param_grid = params[model_name]
+
+            gs = GridSearchCV(model, param_grid, cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+
             model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
@@ -39,7 +46,10 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
 
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
+            report[model_name] = test_model_score
+
+            logging.info(f"{model_name} best params: {gs.best_params_}")
+            logging.info(f"{model_name} R² score: {test_model_score}")
 
         return report
 
@@ -49,7 +59,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
 def load_object(file_path):
     try:
         with open(file_path, "rb") as file_obj:
-            return pickle.load(file_obj)
+            return dill.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
